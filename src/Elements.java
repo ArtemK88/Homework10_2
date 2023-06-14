480abe363ba11db2348f548770e9e1ddda9bf99f
ArtemK88 <kapiton2007@gmail.com>
2023-06-14 21:55:16 +0300

    Homework

--- src/Elements.java
+++ src/Elements.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Elements {
+import java.util.List;
+
+public class Elements {
+    public static void elementsList(List<Integer> list, int x) {
+        int leftSide = 0;
+        int rightSide = list.size() - 1;
+
+        while (leftSide < rightSide) {
+            while (list.get(leftSide) <= x && leftSide < rightSide) {
+                leftSide++;
+            }
+
+            while (list.get(rightSide) > x && leftSide < rightSide) {
+                rightSide--;
+            }
+
+            if (leftSide < rightSide) {
+                int a = list.get(leftSide);
+                list.set(leftSide, list.get(rightSide));
+                list.set(rightSide, a);
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> num = new ArrayList<>(Arrays.asList(5, 2, 6, 4, 5, 3, 8, 7, 9, 10, 10, 12, 13));
+        int x = 6;
+        Elements.elementsList(num, x);
+        System.out.println("Список чисел : " + num);
     }
 }
+
